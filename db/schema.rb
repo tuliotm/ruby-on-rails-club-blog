60e3e628aa4f1a7afe1df4383cb6283d18ccf7bc
tuliotm <tulioteodoromanso@gmail.com>
2024-03-24 14:42:51 -0300

    Using uuid to identify articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_20_225117) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_24_172808) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
+  create_table "articles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
