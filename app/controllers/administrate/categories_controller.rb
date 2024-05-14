3352bf80f6847541ec5f5ffc87990262d278e87d
tuliotm <tulioteodoromanso@gmail.com>
2024-05-14 19:05:45 -0300

    Add Category CRUD for admin - EDIT and SHOW

--- app/controllers/administrate/categories_controller.rb
+++ app/controllers/administrate/categories_controller.rb
@@ -2,7 +2,7 @@
 
 module Administrate
   class CategoriesController < AdministrateController
-    # before_action :set_category, only: [:show, :edit, :update, :destroy, :destroy_cover_image]
+    before_action :set_category, only: [:show, :edit, :update, :destroy, :destroy_cover_image]
     # before_action :set_categories, only: [:new, :edit, :show]
 
     # GET /categories or /categories.json
@@ -10,18 +10,18 @@ module Administrate
       @categories = Category.all
     end
 
-    # # GET /categories/1 or /categories/1.json
-    # def show
-    # end
+    # GET /categories/1 or /categories/1.json
+    def show
+    end
 
     # GET /categories/new
     def new
       @category = Category.new
     end
 
-    # # GET /categories/1/edit
-    # def edit
-    # end
+    # GET /categories/1/edit
+    def edit
+    end
 
     # POST /categories or /categories.json
     def create
@@ -38,44 +38,35 @@ module Administrate
       end
     end
 
-    # # PATCH/PUT /categories/1 or /categories/1.json
-    # def update
-    #   respond_to do |format|
-    #     if @category.update(category_params)
-    #       format.html { redirect_to(administrate_category_url(@category), notice: "Category was successfully updated.") }
-    #       format.json { render(:show, status: :ok, location: @category) }
-    #     else
-    #       format.html { render(:edit, status: :unprocessable_entity) }
-    #       format.json { render(json: @category.errors, status: :unprocessable_entity) }
-    #     end
-    #   end
-    # end
+    # PATCH/PUT /categories/1 or /categories/1.json
+    def update
+      respond_to do |format|
+        if @category.update(category_params)
+          format.html { redirect_to(administrate_category_url(@category), notice: "Category was successfully updated.") }
+          format.json { render(:show, status: :ok, location: @category) }
+        else
+          format.html { render(:edit, status: :unprocessable_entity) }
+          format.json { render(json: @category.errors, status: :unprocessable_entity) }
+        end
+      end
+    end
 
-    # # DELETE /categories/1 or /categories/1.json
-    # def destroy
-    #   @category.destroy!
+    # DELETE /categories/1 or /categories/1.json
+    def destroy
+      @category.destroy!
 
-    #   respond_to do |format|
-    #     format.html { redirect_to(administrate_categories_url, notice: "Category was successfully destroyed.") }
-    #     format.json { head(:no_content) }
-    #   end
-    # end
-
-    # # DELETE /categories/:id/destroy_cover_image
-    # def destroy_cover_image
-    #   @category.cover_image.purge
-
-    #   respond_to do |format|
-    #     format.turbo_stream { render(turbo_stream: turbo_stream.remove(@category)) }
-    #   end
-    # end
+      respond_to do |format|
+        format.html { redirect_to(administrate_categories_url, notice: "Category was successfully destroyed.") }
+        format.json { head(:no_content) }
+      end
+    end
 
     private
 
-    # # Use callbacks to share common setup or constraints between actions.
-    # def set_category
-    #   @category = Category.friendly.find(params[:id])
-    # end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_category
+      @category = Category.find(params[:id])
+    end
 
     # Only allow a list of trusted parameters through.
     def category_params
